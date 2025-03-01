98c2d210217312939bb91bffcd104e5c80618e08
Joel <91616550+Stegi56@users.noreply.github.com>
2025-03-01 17:22:59 +0000

    Refactor from vanilla JS to REACT TS

--- ZeroCam/src/main.js
+++
@@ -1,9 +0,0 @@
-const { invoke } = window.__TAURI__.core;
-
-function scheduleClip() {
-  invoke("feScheduleClip");
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("clip-button").addEventListener("click", scheduleClip);
-});
